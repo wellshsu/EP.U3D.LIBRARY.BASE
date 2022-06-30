f29788ee1fe0020202f4cf3afb1f01125b82fd98
wellshsu <wellshsu@outlook.com>
2022-06-30 21:41:53 +0800

    1.upgrade to 0.0.8 2.change protobuf-net lib, add ilruntime support

---
+++ Runtime/Libs/Protobuf/protobuf-net/IMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace ProtoBuf
+{
+    public interface IMessage
+    {
+        void Encode(Google.Protobuf.CodedOutputStream writer);
+        void Decode(Google.Protobuf.CodedInputStream reader);
+    }
+}
